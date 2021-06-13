fe68ba80295053064ca88b65d48c938d57950724
Adrian <adrian_zinko@op.pl>
2021-06-13 23:41:42 +0200

    19.2. Practice connecting spring to created db

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,23 +1,27 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping(path = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
-    }
+        List<Task> tasks = service.getAllTasks();
 
-    @GetMapping(path = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "task title", "task content");
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @PostMapping(path = "createTask")
